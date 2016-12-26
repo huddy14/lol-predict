de01861f817f65e54c9909ed4daec68d4accb839
pjendrzyca <piotr.jendrzyca@euvic.pl>
2016-12-26 23:49:55 +0100

    initial commit

---
+++ lolpredcit/predict/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+from .service import regions
+from .service import RiotService, ServiceException
+from .naive_bayes import train_naive_bayes
+import os
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'predict/home.html', {'regions':regions})
+
+
+def search(request):
+    if request.method == 'GET':
+        #return render(request, 'predict/loading.html')
+        try:
+            api = RiotService(request.GET['region'])
+            summoner_id = api.get_summoner_id(request.GET['summoner'])
+            current_match = api.get_current_game(summoner_id)
+        except ServiceException as e:
+            return render(request, 'predict/error.html', {'message': str(e), 'regions':regions})
+        data = api.get_data_from_current_match(current_match)
+        predict = data['data']
+        players = data['players']
+        red = []
+        blue = []
+
+        clf = train_naive_bayes(os.getcwd()+'/database/matchData')
+
+        probability = clf.predict_proba(predict)[0]
+        probability[0] = round(probability[0] * 100, 2)
+        probability[1] = round(probability[1] * 100, 2)
+
+        for player in players:
+            if player.team == 200:
+                red.append(player)
+            elif player.team == 100:
+                blue.append(player)
+        return render(request, 'predict/search.html', {'name':request.GET['summoner'], 'region':request.GET['region'], 'regions':regions, 'red':red, 'blue':blue, 'prediction':probability})
+
+    return render(request, 'predict/search.html', {'name': 'nie dziala post'})
